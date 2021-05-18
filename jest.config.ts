bdc0ae26ad34ff0edd764e89d3ec551d75304e0f
samtade <supacheep.amtade@agoda.com>
2021-05-18 09:47:21 +0700

    move jest config to `jest.config.ts`

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+const e2eConfig: Config.InitialOptions = {
+  ...config,
+  rootDir: 'test',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+};
+
+const isE2E = process.env.TEST_ENV === 'e2e';
+
+export default isE2E ? e2eConfig : config;

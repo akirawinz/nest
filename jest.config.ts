c43a5ea875fbaca6f59372423eda7ae0bd68f115
samtade <supacheep.amtade@agoda.com>
2021-05-18 15:27:49 +0700

    fix jest config to support module mapping

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,30 @@
 // jest.config.ts
 import type { Config } from '@jest/types';
 
-// Sync object
-const config: Config.InitialOptions = {
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+import * as tsconfig from './tsconfig.json';
+
+interface TsConfig {
+  compilerOptions: {
+    paths: {
+      [k: string]: string[];
+    };
+  };
+}
+
+// Slightly modified version of https://github.com/ryohey/tsconfig-paths-jest
+function moduleNameMapperFromTSPaths(tsconfig: TsConfig) {
+  // eslint-disable-next-line prettier/prettier
+  return Object.entries(tsconfig.compilerOptions.paths)
+      // eslint-disable-next-line prettier/prettier
+      .map(([k, [v]]) => [`^${k.replace(/\*/, '(.*)')}$`, `<rootDir>/../${v.replace(/\*/, '$1')}`])
+      // eslint-disable-next-line prettier/prettier
+      .reduce((res, [key, value]) => ({ ...res, [key]: value }), {});
+}
+
+// base jest config
+const baseConfig: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
@@ -12,10 +34,12 @@ const config: Config.InitialOptions = {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: moduleNameMapperFromTSPaths(tsconfig),
 };
 
+// override for e2e
 const e2eConfig: Config.InitialOptions = {
-  ...config,
+  ...baseConfig,
   rootDir: 'e2e',
   testEnvironment: 'node',
   testRegex: '.e2e-spec.ts$',
@@ -23,4 +47,8 @@ const e2eConfig: Config.InitialOptions = {
 
 const isE2E = process.env.TEST_ENV === 'e2e';
 
-export default isE2E ? e2eConfig : config;
+const config = isE2E ? e2eConfig : baseConfig;
+
+console.log('jest config', config);
+
+export default config;

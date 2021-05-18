65d3d60f8ffa6f8eac706217aeadf3848a459343
samtade <supacheep.amtade@agoda.com>
2021-05-18 13:04:12 +0700

    keep private inside file

--- src/common/utils/file-uploading.utils.ts
+++ src/common/utils/file-uploading.utils.ts
@@ -1,15 +1,14 @@
 import { extname } from 'path';
 import { diskStorage } from 'multer';
 
-export const imageFileFilter = (req, file, callback) => {
+const filterImageFile = (req, file, callback) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
     return callback(new Error('Only image files are allowed!'), false);
   }
   callback(null, true);
 };
 
-export const editFileName = (req, file, callback) => {
-  console.log(file, 'file');
+const buildFileName = (req, file, callback) => {
   const name = file.originalname.split('.')[0] || file.name.split('.')[0];
   const fileExtName = extname(file.originalname || file.name);
   const randomName = Array(4)
@@ -23,8 +22,8 @@ export const uploadImagePath = () => {
   return {
     storage: diskStorage({
       destination: './upload',
-      filename: editFileName,
+      filename: buildFileName,
     }),
-    fileFilter: imageFileFilter,
+    fileFilter: filterImageFile,
   };
 };
